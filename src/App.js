c49d11f12a5be90de56b1527ee2c42f02041f66f
kishorekuraganti <krishnakishore9347@gmail.com>
2023-10-23 16:26:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>Kishore Kuraganti</h1>
+        <p>Web Developer | Data Science Enthusiast</p>
       </header>
+
+      <section className="about-me">
+        <h2>About Me</h2>
+        <br></br>
+        <br></br>
+        <section id="id1">
+          Hi! I'm Kishore Kuraganti, a intrested web developer and Data Science
+          enthusiast. I am from Rajahmundry, Andhra Pradesh. I'm currently
+          pursuing B.Tech in Computer Science. I completed my Intermediate
+          Narayana Junior College,RJY. I am Enthusiastic Computer Science
+          student with a passion for learning and applying emerging
+          technologies.Strong problem-solving skills and a collaborative
+          mindset, eager to contribute to innovative software solutions.
+        </section>
+      </section>
+
+      <section className="skills">
+        <h2>Skills</h2>
+        <br></br>
+        <br></br>
+        <ul>
+          <li>Java</li>
+          <li>React</li>
+          <li>Python</li>
+          <li>R programming</li>
+          <li>HTML & CSS</li>
+          <li>DSA</li>
+        </ul>
+      </section>
+
+      <section className="projects">
+        <h2>Projects</h2>
+        <br></br>
+        <br></br>
+        <ul>
+          <li>Solar pannel Sun Tracker</li>
+          <li>Driver Drowsiness Detection System</li>
+          <li>Google play store data Analysis</li>
+        </ul>
+      </section>
+
+      <section className="contact">
+        <h2>Contact</h2>
+        <br></br>
+        <br></br>
+        <p>
+          Email:{" "}
+          <a href="mailto:krishnakishore9347@gmail.com">
+            krishnakishore9347@gmail.com
+          </a>
+        </p>
+        <p>
+          LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/kishore-kuraganti-669b70188">
+            LinkedIn
+          </a>
+        </p>
+        <p>
+          GitHub: <a href="https://github.com/kishorekuraganti">Github</a>
+        </p>
+      </section>
     </div>
   );
 }
-
-export default App;
